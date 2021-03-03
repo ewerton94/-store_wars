43e4b3c8073d609978abcd6a6a50d7076643debe
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-03 16:26:54 -0300

    feat: Initial config #2

--- StoreWars/src/boot/axios.js
+++ StoreWars/src/boot/axios.js
@@ -1,4 +1,16 @@
-import Vue from 'vue'
 import axios from 'axios'
 
-Vue.prototype.$axios = axios
+// const baseURL = 'http://localhost:8000/'
+const baseURL = 'https://ewerton94.pythonanywhere.com/'
+
+const HTTPClient = axios.create({
+  baseURL: baseURL
+})
+
+// HTTPClient.defaults.withCredentials = true
+HTTPClient.defaults.xsrfCookieName = 'csrftoken'
+HTTPClient.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+export {
+  HTTPClient
+}
