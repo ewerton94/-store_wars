a0810307a8d987636bb0ae1234ecd4e083201fbb
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-05 16:45:50 -0300

    feat: Create Getters for orders #5

--- StoreWars/src/store/Orders/getters.js
+++ StoreWars/src/store/Orders/getters.js
@@ -1,4 +1,29 @@
-/*
-export function someGetter (state) {
+
+const getOrderById = (state, orderId) => {
+  if (orderId === undefined) {
+    return state.order
+  } else {
+    return state.orders.find((o) => o.id === orderId)
+  }
 }
-*/
+
+const numberOrders = (state, getters) => {
+  return state.orders.length
+}
+
+const numberItemsByOrder = (state, getters) => (orderId) => {
+  var order = getOrderById(state, orderId)
+  return order.items.length
+}
+
+const valueTotalByOrder = (state, getters) => (orderId) => {
+  var order = getOrderById(state, orderId)
+  return order.items.map((i) => Number(i.price)).reduce((a, b) => a + b, 0)
+}
+
+export {
+  numberOrders,
+  numberItemsByOrder,
+  valueTotalByOrder
+
+}
