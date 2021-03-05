ee01eefafaa8e5cc66eda663466d8b0b913bb7c6
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-04 21:53:32 -0300

    feat: Base for Homepage #9

---
+++ StoreWars/src/components/Home/BreadCrumbs.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="q-pa-md">
+    <div class="bg-dark text-white ">
+      <q-toolbar >
+        <q-breadcrumbs active-color="cyan-13" style="font-size: 16px">
+          <q-breadcrumbs-el label="Orders" icon="store" />
+          <q-breadcrumbs-el label="Nova" icon="add" />
+
+        </q-breadcrumbs>
+      </q-toolbar>
+      <q-toolbar >
+        <q-btn flat round dense icon="store" />
+
+        <q-toolbar-title>Orders</q-toolbar-title>
+
+        <q-btn flat class="bg-grey-9 q-px-sm text-white" dense label="Nova ordem" icon="add" />
+      </q-toolbar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EssentialLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    caption: {
+      type: String,
+      default: ''
+    },
+
+    link: {
+      type: String,
+      default: '#'
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ StoreWars/src/components/Home/FilterBlock.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Filter</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
