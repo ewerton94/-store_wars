af525eb73fec1d72dec29cae6a61bcbaa7dc14a4
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-05 17:23:35 -0300

    feat: New page for new Orders #7

---
+++ StoreWars/src/pages/NewOrder.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page class="WAL__layout " >
+    <div class="row q-ma-md">
+      <div class="full-width">
+        <Breadcrumbs :pages="pages"  />
+      </div>
+      <div  class="full-width">
+
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import Breadcrumbs from 'components/Home/Breadcrumbs.vue'
+
+export default {
+  name: 'PageIndex',
+  data: function () {
+    return {
+      pages: [
+        { name: 'Pedidos', icon: 'store', href: '/' },
+        { name: 'Novo Pedido', icon: 'add', href: '/new-order' }
+      ]
+    }
+  },
+  components: {
+    Breadcrumbs
+
+  }
+}
+</script>
+<style  lang="sass">
+.WAL
+  &__layout
+    margin: 0 auto
+    height: 100%
+    width: 90%
+    max-width: 1050px
+    border-radius: 5px
+@media (max-width: 850px)
+  .WAL
+    padding: 0
+    &__layout
+      width: 100%
+      border-radius: 0
+@media (min-width: 691px)
+  .WAL
+    &__drawer-open
+      display: none
+</style>
