3002e5327885d824d5989849e3aa674d868805bb
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-05 21:27:32 -0300

    feat: Create Order page #6

--- StoreWars/src/components/Home/ListOrders.vue
+++ StoreWars/src/components/Home/ListOrders.vue
@@ -1,25 +1,38 @@
 <template>
   <div>
-    <Order
-    v-for="(order, index) in orders"
-    :key="index"
-    :order="order"
-    />
+    <div v-if="!carregando">
+      <Order
+        v-for="(order, index) in orders"
+        :key="index"
+        :order="order"
+      />
+    </div>
+    <ListaVazia v-if="!carregando && !orders.length" message="Sem Pedidos Cadastrados" />
   </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex'
+import ListaVazia from 'components/ListaVazia.vue'
 import Order from './Order.vue'
+import { Loading } from 'quasar'
 
 export default {
+  data: function () {
+    return {
+      carregando: true
+    }
+  },
   computed: {
     ...mapState('Orders', ['orders'])
   },
   methods: {
     ...mapActions('Orders', ['listOrders']),
     async getorders () {
+      Loading.show()
       await this.listOrders()
+      Loading.hide()
+      this.carregando = false
       console.log(this.orders)
     }
   },
@@ -27,7 +40,8 @@ export default {
     this.getorders()
   },
   components: {
-    Order
+    Order,
+    ListaVazia
   }
 }
 </script>
--- StoreWars/src/components/NewOrder/OrderForm.vue
+++ StoreWars/src/components/NewOrder/OrderForm.vue
@@ -1,27 +1,49 @@
 <template>
   <div>
-      {{ order }}
-      <Select
-        :options="customers"
-        v-model="order.customer"
-        label="Cliente"
-        campo_label="name"
-        icon="person"
-        v-if="customers.length"
+      <p>{{ order.items.length }} Itens</p>
+    <q-form @submit="sendOrder">
 
-      />
+    <Select
+      :options="customers"
+      v-model="order.customer"
+      label="Cliente"
+      campo_label="name"
+      icon="person"
 
-      <div v-if="order.items">
+      reactive-rules
 
+      :lazy_rules="true"
+      required
+      :rules="[val => val > 0 || 'Insira um cliente']"
+
+      v-if="customers.length"
+    />
+
+    <div v-if="order.items">
       <div v-for="(item, index) in order.items" :key="index">
-          {{ order.items[index] }}
-          {{ index }}
-          <ItemForm v-model="order.items[index]" :products="products"/>
+        <ItemForm v-model="order.items[index]" @input="$emit('input', $event)" :products="products" />
       </div>
-
-      <q-btn icon="add" label="Adicionar item" @click="newItem"></q-btn>
-
+      <div class="row justify-end">
+        <q-btn
+          align="right"
+          class="bg-grey-9 q-px-sm text-white"
+          icon="add"
+          label="Adicionar item"
+          @click="newItem"
+        ></q-btn>
       </div>
+    </div>
+
+    <div class="row justify-end">
+        <q-btn
+          align="right"
+          class="q-mt-md bg-cyan-12 q-px-sm text-black"
+          icon="check"
+          label="Enviar"
+          type="submit"
+        ></q-btn>
+      </div>
+      </q-form>
   </div>
 </template>
 
@@ -29,6 +51,7 @@
 import Select from './Select.vue'
 import ItemForm from './ItemForm.vue'
 import { mapActions, mapState } from 'vuex'
+import { Notify } from 'quasar'
 
 export default {
   data: function () {
@@ -52,8 +75,27 @@ export default {
     newItem () {
       this.order.items.push({
         product: null
-
       })
+    },
+    check () {
+      if (this.order.items.length === 0) {
+        Notify.create({ type: 'negative', message: 'Adicione pelo menos um item' })
+        return false
+      }
+      if (this.order.items.filter((i) => i.profitability === 'bad').length > 0) {
+        Notify.create({ type: 'negative', message: 'Verifique os valores de preço.' })
+        return false
+      }
+      if (this.order.items.filter((value) => value.quantity % this.products.find((p) => p.id === value.product).multiple !== 0).length > 0) {
+        Notify.create({ type: 'negative', message: 'Verifique os valores de quantidade.' })
+        return false
+      }
+      return true
+    },
+    sendOrder () {
+      if (this.check()) {
+        console.log('OK')
+      }
     }
   },
   mounted () {
@@ -63,13 +105,9 @@ export default {
     Select,
     ItemForm
   },
-  props: [
-    'id'
-  ]
-
+  props: ['id']
 }
 </script>
 
 <style>
-
 </style>
