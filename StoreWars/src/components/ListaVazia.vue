f7cedd58cc5c9d3e90819ea320e3754eaf25553d
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-05 21:27:52 -0300

    feat: Empty list

---
+++ StoreWars/src/components/ListaVazia.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="">
+    <div class="row text-center">
+      <div class="col col-12 col-md-12">
+        <q-icon size="70px" color="blue-5" name="warning" />
+      </div>
+    </div>
+    <br />
+    <div class="row text-center">
+      <div class="col col-12 col-md-12">
+        <p class="text-blue-5">{{ message }}</p>
+      </div>
+    </div>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message']
+}
+</script>
+
+<style>
+.hh2 {
+  height: 100%;
+  min-height: 50vh !important;
+}
+</style>
