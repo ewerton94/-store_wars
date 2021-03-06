461dac8a7c58a72d45cf9e54705a3852d496d171
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-05 22:56:23 -0300

    feat: Order CRUD #12

--- StoreWars/src/store/Orders/mutations.js
+++ StoreWars/src/store/Orders/mutations.js
@@ -1,7 +1,22 @@
 const SET_ORDERS = (state, payload) => {
   state.orders = payload
 }
+const SET_ORDER = (state, payload) => {
+  state.orderUpdate = payload
+}
+
+const DELETE_ORDER = (state, id) => {
+  var idRemover = 0
+  state.orders.forEach((f, i) => {
+    if (f.id === id) {
+      idRemover = i
+    }
+  })
+  state.orders.splice(idRemover, 1)
+}
 
 export {
-  SET_ORDERS
+  SET_ORDERS,
+  SET_ORDER,
+  DELETE_ORDER
 }
--- StoreWars/src/store/Orders/state.js
+++ StoreWars/src/store/Orders/state.js
@@ -1,5 +1,6 @@
 export default function () {
   return {
-    orders: []
+    orders: [],
+    orderUpdate: {}
   }
 }
